e3485885289e37410f2d2ab7e46f3c9a48ccb0f1
Areydra Desfikriandre <areydra@gmail.com>
2025-09-25 07:33:32 +0700

    refactor(config): migrate app config from json to typescript
    Move app configuration from app.json to app.config.ts for better type safety and maintainability

---
+++ app.config.ts
@@ -0,0 +1,77 @@
+export default {
+  expo: {
+    name: "Matur",
+    slug: "Matur",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/images/icon.png",
+    scheme: "matur",
+    userInterfaceStyle: "automatic",
+    newArchEnabled: true,
+    ios: {
+      supportsTablet: true,
+      bundleIdentifier: "com.matur",
+      appleTeamId: process.env.EXPO_APPLE_TEAM_ID
+    },
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/images/icon.png",
+        backgroundColor: "#FFFFFF"
+      },
+      edgeToEdgeEnabled: true,
+      package: "com.matur"
+    },
+    web: {
+      bundler: "metro",
+      output: "static",
+      favicon: "./assets/images/icon.png"
+    },
+    plugins: [
+      "expo-router",
+      [
+        "expo-splash-screen",
+        {
+          image: "./assets/images/icon-with-brand.png",
+          imageWidth: 200,
+          resizeMode: "contain",
+          backgroundColor: "#0E0A47"
+        }
+      ],
+      [
+        "expo-build-properties",
+        {
+          ios: {
+            useFrameworks: "static",
+            extraPods: [
+              {
+                name: "simdjson",
+                configurations: [
+                  "Debug",
+                  "Release"
+                ],
+                path: "../node_modules/@nozbe/simdjson",
+                modular_headers: true
+              }
+            ]
+          }
+        }
+      ],
+      [
+        "@react-native-google-signin/google-signin",
+        {
+          iosUrlScheme: process.env.EXPO_PUBLIC_IOS_URL_SCHEME
+        }
+      ],
+      [
+        "expo-image-picker",
+        {
+          photosPermission: "The app would like to access your photos to allow you to select a profile picture.",
+          cameraPermission: "The app would like to access your camera to allow you to take a profile picture."
+        }
+      ]
+    ],
+    experiments: {
+      typedRoutes: true
+    }
+  }
+};
